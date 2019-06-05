2246ce9cc792fff03b48b4ace31e5fe3c6a65498
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-05 14:29:03 +0900

    e-groups

--- src/config.js
+++ src/config.js
@@ -1,12 +1,11 @@
-// Replace this section with the config file of your own Firebase project. 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8Gvm5JpOY6QlignjLSs-V2iNkQo-VVGQ",
-  authDomain: "skelton-us.firebaseapp.com",
-  databaseURL: "https://skelton-us.firebaseio.com",
-  projectId: "skelton-us",
-  storageBucket: "skelton-us.appspot.com",
-  messagingSenderId: "1041354638035",
-  appId: "1:1041354638035:web:9e02a6ea52daf17f"
+  apiKey: "AIzaSyDjiYcArEzy5_0kc1Bq6uo0pHKrDYpBWKY",
+  authDomain: "e-groups.firebaseapp.com",
+  databaseURL: "https://e-groups.firebaseio.com",
+  projectId: "e-groups",
+  storageBucket: "e-groups.appspot.com",
+  messagingSenderId: "866990675127",
+  appId: "1:866990675127:web:821b9610148c3b2b"
 };
 
 export default firebaseConfig;
