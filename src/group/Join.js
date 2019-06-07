943316409b294069389c0d420e13c2d839071ce1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-07 11:32:44 -0700

    unable to join

--- src/group/Join.js
+++ src/group/Join.js
@@ -12,15 +12,22 @@ const styles = theme => ({
 });
 
 class Join extends React.Component {
+  state = { error:null };
   handleJoin = async () => {
     const { db, user, group } = this.props;
     const refMember = db.doc("groups/" + group.groupId + "/members/" + user.uid);
-    await refMember.set({ lastAccessed: firebase.firestore.FieldValue.serverTimestamp()});
-    this.props.memberDidUpdate();
-    window.location.pathname = "/" + group.groupName + "/account";
+    try {
+        await refMember.set({ lastAccessed: firebase.firestore.FieldValue.serverTimestamp()});
+        this.props.memberDidUpdate();
+        window.location.pathname = "/" + group.groupName + "/account";
+    } catch(e) {
+        console.log(e);
+        this.setState({error:"Unable to Join"})
+    }
   }
   render() {
     const { user, classes, group } = this.props;
+    const { error } = this.state;
     if (!user) {
         return <Typography variant="h5" className={classes.login}>
             In order to join {group.title}, please create your account by choosing Login first.
@@ -30,6 +37,9 @@ class Join extends React.Component {
         return <div>
             <Typography>This community is invitation only.</Typography>
             <Button variant="contained" onClick={this.handleJoin}>Try to Join</Button>
+            {
+                error && <p style={{color:"red"}}>{error}</p>
+            }
         </div>
 
     }
