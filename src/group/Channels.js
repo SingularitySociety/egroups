de10da8669ee2a033a4eac61dc1c6c926649f649
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-07 15:09:03 -0700

    Channels

--- src/group/Channels.js
+++ src/group/Channels.js
@@ -8,28 +8,50 @@ const styles = theme => ({
 });
 
 class Channels extends React.Component {
-    createChannel = async () => {
-        console.log("createChannel");
-        const { db, group } = this.props;
-        db.collection(`groups/${group.groupId}/channels`).add({
-          title: "new Channel"
-        });
-      }
-        render() {
-            const { classes } = this.props;
-            return <div>
-          <Typography component="h2" variant="h6" gutterBottom className={classes.welcome}>
-            Channels
-          </Typography>
-          <IconButton variant="contained" className={classes.button} onClick={this.createChannel}>
-            <AddIcon />
-          </IconButton>
-        </div>
-    }
+  state = { list:[] }
+  componentDidMount() {
+    const { db, group } = this.props;
+    this.detacher = db.collection(`groups/${group.groupId}/channels`).onSnapshot((snapshot) => {
+      console.log("onSnapshot")
+      const list = [];
+      snapshot.forEach((doc)=>{
+        const channel = doc.data();
+        channel.channelId = doc.id;
+        list.push(channel);
+      });
+      this.setState({list});
+    })
+  }
+  componentWillUnmount() {
+    this.detacher();
+  }
+  createChannel = async () => {
+    console.log("createChannel");
+    const { db, group } = this.props;
+    db.collection(`groups/${group.groupId}/channels`).add({
+      title: "new Channel"
+    });
+  }
+  render() {
+    const { classes } = this.props;
+    return <div>
+      <Typography component="h2" variant="h6" gutterBottom className={classes.welcome}>
+        Channels
+      </Typography>
+      <IconButton variant="contained" className={classes.button} onClick={this.createChannel}>
+        <AddIcon />
+      </IconButton>
+      <div>
+        { this.state.list.map((channel)=>{
+          return <div key={channel.channelId}>{channel.title}</div>
+        })}
+      </div>
+    </div>
+  }
 }
 
 Channels.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+  classes: PropTypes.object.isRequired,
+};
   
 export default withStyles(styles)(Channels);
