94e30652cf62a756ef3e034b9ff0d93f7beee051
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-08 10:06:46 -0700

    channel name

--- src/group/Channels.js
+++ src/group/Channels.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, IconButton } from '@material-ui/core';
+import { Typography, IconButton, Button, TextField } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 
 const styles = theme => ({
 });
 
 class Channels extends React.Component {
-  state = { list:[] }
+  state = { list:[], creating:false, value:"" }
   componentDidMount() {
     const { db, group } = this.props;
     this.detacher = db.collection(`groups/${group.groupId}/channels`).onSnapshot((snapshot) => {
@@ -25,22 +25,39 @@ class Channels extends React.Component {
   componentWillUnmount() {
     this.detacher();
   }
-  createChannel = async () => {
-    console.log("createChannel");
+  createChannel = async (e) => {
+    e.preventDefault();
     const { db, group } = this.props;
     db.collection(`groups/${group.groupId}/channels`).add({
-      title: "new Channel"
+      title: this.state.value
     });
+    this.setCreatingFlag(false);
+  }
+  setCreatingFlag = (creating) => {
+    this.setState({creating, value:""})
+  }
+  onChange = (e) => {
+    let value = e.target.value;
+    this.setState({value});
   }
   render() {
     const { classes } = this.props;
+    const { creating, value } = this.state;
     return <div>
       <Typography component="h2" variant="h6" gutterBottom className={classes.welcome}>
         Channels
       </Typography>
-      <IconButton variant="contained" className={classes.button} onClick={this.createChannel}>
-        <AddIcon />
-      </IconButton>
+        { creating ?
+          <form>
+            <TextField onChange={this.onChange} value={value} autoFocus />
+            <Button variant="contained" onClick={this.createChannel} type="submit">Create</Button>
+            <Button variant="contained" onClick={()=>this.setCreatingFlag(false)}>Cancel</Button>
+          </form>
+          : 
+          <IconButton variant="contained" onClick={()=>this.setCreatingFlag(true)}>
+            <AddIcon />
+          </IconButton>
+      }
       <div>
         { this.state.list.map((channel)=>{
           return <div key={channel.channelId}>{channel.title}</div>
