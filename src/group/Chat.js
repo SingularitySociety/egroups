dce11c70de562cf1bb9747d969d25723651fe6b8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-09 05:59:52 -0700

    can Chat now

--- src/group/Chat.js
+++ src/group/Chat.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import CreateNew from './CreateNew';
 
 const styles = theme => ({
 });
@@ -15,7 +18,8 @@ class Chat extends React.Component {
     const channel = (await ref.get()).data();
     this.setState({channel});
     console.log(channel);
-    this.detacher = ref.collection("messages").onSnapshot((snapshot)=>{
+    this.refMessages = ref.collection("messages");
+    this.detacher = this.refMessages.orderBy("created").onSnapshot((snapshot)=>{
       const messages=[];
       snapshot.forEach((doc) => {
         const message = doc.data();
@@ -23,22 +27,43 @@ class Chat extends React.Component {
         messages.push(message);
       })
       this.setState({messages});
+      console.log(messages);
     });
   }
 
   componentWillUnmount() {
     this.detacher && this.detacher();
   }
+  
+  postMessgae = async (message) => {
+    const { user, member } = this.props;
+    await this.refMessages.add({
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      message,
+      userid: user.uid,
+      userName: member.displayName
+    });
+  }
 
   render() {
-    const { channel } = this.state;
+    const { channel, messages } = this.state;
     const { member } = this.props;
-    const canAccessChannel = ((member && member.privilege) || 0) >= ((channel && channel.privilege) || 1);
-    return (
+    if (!channel) {
+      return "";
+    }
+    const canRead = ((member && member.privilege) || 0) >= channel.read;
+    const canWrite = ((member && member.privilege) || 0) >= channel.write;
+    return (<div>
       <Typography component="h2" variant="h5" gutterBottom>
         { channel.title }
       </Typography>
-    )
+      <div>
+      { messages.map((message)=>{
+        return <div key={message.messageId}>{message.message}</div>
+      }) }
+      { canWrite && <CreateNew createNew={ this.postMessgae }/> }
+      </div>
+    </div>)
   }
 }
 
