304315bcb7f5bf4a56f0c83740a3aa7faef15e68
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-08 21:06:10 -0700

    channel title

--- src/group/Chat.js
+++ src/group/Chat.js
@@ -7,12 +7,22 @@ const styles = theme => ({
 });
 
 class Chat extends React.Component {
+  state = {channel:{}}
+  async componentDidMount() {
+    const { db, group, match:{params:{channelId}} } = this.props;
+    console.log(channelId);
+    const ref = db.doc(`groups/${group.groupId}/channels/${channelId}`);
+    const channel = (await ref.get()).data();
+    console.log(channel);
+    this.setState({channel:channel})
+  }
     render() {
-        return (
-            <Typography component="h2" variant="h5" gutterBottom>
-              To be implemented
-            </Typography>
-          )
+      const { channel } = this.state;
+      return (
+        <Typography component="h2" variant="h5" gutterBottom>
+          { channel.title }
+        </Typography>
+      )
     }
 }
 
