5d248ca089f78e650248e972261de12d811ba8aa
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-11 15:02:55 -0700

    rules cleanup, word-wrap

--- src/group/Message.js
+++ src/group/Message.js
@@ -6,6 +6,12 @@ import { Typography } from '@material-ui/core';
 const styles = theme => ({
   userName: {
     color: "#606060",
+  },
+  message: {
+    wordWrap: "break-word",
+  },
+  frame: {
+    width: "calc(95vmin)",
   }
 });
 
@@ -13,11 +19,11 @@ class Message extends React.Component {
   render() {
     const { message, classes } = this.props;
     return (
-      <div>
+      <div className={classes.frame}>
         <Typography variant="caption" className={classes.userName} gutterBottom>
           { message.userName }
         </Typography>
-        <Typography gutterBottom>
+        <Typography gutterBottom className={classes.message}>
           { message.message }
         </Typography>
       </div>
