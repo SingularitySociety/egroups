0e4f302c0fe87a2bcffcb4e93557cf0f67ca922a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-11 22:15:55 -0700

    convertToRaw

--- src/common/DraftEditor.js
+++ src/common/DraftEditor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Editor, EditorState　} from 'draft-js';
+import { Editor, EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 import theme from '../theme';
 import { Button } from '@material-ui/core';
 import { FormattedMessage } from 'react-intl';
@@ -29,17 +29,34 @@ class DraftEditor extends React.Component {
     console.log("SectionEditor:didMount");
 
   }
+  handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.content = convertToRaw(this.state.editorState.getCurrentContent());
+    console.log(this.content);
+  }
+  onCancel = (e) => {
+    const editorState = EditorState.createWithContent(convertFromRaw(this.content));
+    this.setState({editorState});
+  }
   render() {
     const { classes, action } = this.props;
     return (
       <div>
         <div className={classes.frame}>
-          <Editor editorState={this.state.editorState} onChange={this.onChange} />
+          <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange} />
         </div>
         
         <Button variant="contained" color="primary" className={classes.button} 
-                  onClick={this.onSubmit} type="submit">{action || "Create"}</Button>
-        <Button variant="contained" className={classes.button} onClick={()=>this.setCreatingFlag(false)}>
+                  onClick={this.onSubmit} type="submit">{action || "Save"}</Button>
+        <Button variant="contained" className={classes.button} onClick={this.onCancel}>
             <FormattedMessage id="cancel" />
         </Button>
         </div>
