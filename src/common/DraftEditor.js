ef1487d36ffe381c8f7251477ec141d6bae645cf
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-11 21:40:08 -0700

    buttons

--- src/common/DraftEditor.js
+++ src/common/DraftEditor.js
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Editor, EditorState　} from 'draft-js';
 import theme from '../theme';
+import { Button } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
 
 const styles = {
   frame: {
       border: "1px solid #d0d0d0",
       padding: theme.spacing(1),
       borderRadius: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+  },
+  button: {
+    marginRight: theme.spacing(1),
   }
 };
 
@@ -24,10 +30,18 @@ class DraftEditor extends React.Component {
 
   }
   render() {
-    const { classes } = this.props;
+    const { classes, action } = this.props;
     return (
+      <div>
         <div className={classes.frame}>
-        <Editor style={{background:"yellow", width:100, height:100}} editorState={this.state.editorState} onChange={this.onChange} />
+          <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        </div>
+        
+        <Button variant="contained" color="primary" className={classes.button} 
+                  onClick={this.onSubmit} type="submit">{action || "Create"}</Button>
+        <Button variant="contained" className={classes.button} onClick={()=>this.setCreatingFlag(false)}>
+            <FormattedMessage id="cancel" />
+        </Button>
         </div>
     );
   }
