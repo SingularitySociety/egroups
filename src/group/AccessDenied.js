3e660b98b2f615fbd5dcca91eb061ad4912989de
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-12 21:51:55 -0700

    error handling

--- src/group/AccessDenied.js
+++ src/group/AccessDenied.js
@@ -2,15 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
 
 const styles = theme => ({
+  errorMessage: {
+    color: "#ff8000",
+  }
 });
 
 class AccessDenied extends React.Component {
   render() {
+    const {classes} = this.props;
     return (
-      <Typography component="h2" variant="h5" gutterBottom>
-        Access Denied
+      <Typography component="h2" variant="h5" gutterBottom　className={ classes.errorMessage} >
+        <FormattedMessage id="warning.access.denied" />
       </Typography>
     )
   }
--- src/group/Blog.js
+++ src/group/Blog.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
-//import { FormattedMessage } from 'react-intl';
+import AccessDenied from './AccessDenied';
 import BlogSection from './BlogSection';
+import ErrorMessage from '../ErrorMessage';
 
 const styles = theme => ({
 });
@@ -14,6 +15,12 @@ class Blog extends React.Component {
     const { db, group, match:{params:{articleId}} } = this.props;
     this.refArticle = db.doc(`groups/${group.groupId}/articles/${articleId}`);
     const article = (await this.refArticle.get()).data();
+    console.log("article=", article);
+    // BUGBUG: This is an attempt to catch non-existing but does not work because it causes security error unlike chat. 
+    if (article === null) {
+      this.setState({error:{key:"error.invalid.articleId", value:articleId}});
+      return;
+    }
     this.setState({article});
     this.detatcher = this.refArticle.collection("sections").onSnapshot((snapshot)=>{
       const resources = {};
@@ -53,12 +60,22 @@ class Blog extends React.Component {
   }
 
   render() {
-    const { article, resources } = this.state;
-    const { user } = this.props;
-    if (!resources || !user) {
+    const { article, resources, error } = this.state;
+    const { user, member } = this.props;
+    if (error) {
+      return <ErrorMessage error={error} />
+    }
+    if (!user || !article) {
       return "";
     }
     const canEdit = (article.owner === user.uid);
+    const canRead = ((member && member.privilege) || 0) >= article.read;
+    if (!canRead) {
+      return <AccessDenied />
+    }
+    if (!resources) {
+      return "";
+    }
     return (
       <div>
         <Typography component="h2" variant="h5" gutterBottom>
