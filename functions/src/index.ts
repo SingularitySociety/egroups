d2274dd876bf6bd111767e42b22a6c389b9d4b3a
Isamu Arimoto <isamu@to-kyo.to>
2019-06-16 08:21:00 +0900

    add subscribe token

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -68,28 +68,45 @@ export const messageDidCreate = functions.firestore.document('groups/{groupId}/c
 
 export const tokenDidCreate = functions.firestore.document('users/{userId}/private/tokens')
   .onWrite((change, context) => {
-    const newDocument = change.after.exists ? ((change.after.data() || {}).tokens || []) : [];
-    const oldDocument = change.before ? ((change.before.data() || {}).tokens || []) : [];
-    // see diff
-    if (newDocument.length === oldDocument.length) {
-      return;
-    }
-    
-    // get all groups
-    // const groupsRef = db.collectionGroup('members').where(‘uid’, ‘==’, user.uid); 
-    
-    if (newDocument.length > oldDocument.length) {
-      // add
-      const diff = utils.array_diff(newDocument, oldDocument);
-      console.log(diff);
-    } 
-    if (newDocument.length < oldDocument.length) {
-      //remove
-      const diff = utils.array_diff(oldDocument, newDocument);
-      console.log(diff);
-    } 
+    const { userId } = context.params;
+    const newTokens = change.after.exists ? ((change.after.data() || {}).tokens || []) : [];
+    const oldTokens = change.before ? ((change.before.data() || {}).tokens || []) : [];
 
-    // subscribe topic
-    
+    const db = admin.firestore();
+    subscribe_topic(newTokens, oldTokens, userId, db, (tokens, topic) => {
+      try {
+        const response = admin.messaging().subscribeToTopic(tokens, topic);
+        console.log('Successfully subscribed to topic:', response);
+      } catch(error) {
+        console.log('Error subscribing to topic:', error);
+      }
+    });
   });
 
+export const subscribe_topic = async (newTokens, oldTokens, userId, db, subscribe) => {
+  // see diff
+  if (newTokens.length === oldTokens.length) {
+    return;
+  }
+  
+  // get all groups
+  const groupsSnapShot = await db.collectionGroup('members').where("uid", "==", "alice").get()
+  const topics = groupsSnapShot.docs.map((doc) => {
+    return "g_" + doc.data().groupId;
+  });
+  
+  if (newTokens.length > oldTokens.length) {
+    // add
+    const tokens = utils.array_diff(newTokens, oldTokens);
+    topics.forEach((topic) => {
+      subscribe(tokens, topic);
+    });
+  }
+  if (newTokens.length < oldTokens.length) {
+    //remove
+    const diff = utils.array_diff(oldTokens, newTokens);
+    console.log(diff);
+  } 
+  return
+
+}
---
+++ functions/test/store_test.ts
@@ -0,0 +1,43 @@
+import * as test_helper from "../../lib/test/rules/test_helper";
+import * as firebase from "@firebase/testing";
+import { should } from 'chai';
+
+import * as index from '../src/index';
+
+should()
+
+const admin_db = test_helper.adminDB();
+
+const aliceGroupId = "alice_group";
+const aliceUID = "alice";
+
+describe("test topic subscription", () => {
+  it("should subscribe topic", async () => {
+    const alice_group = admin_db.doc(`groups/${aliceGroupId}`);
+    // await firebase.assertSucceeds(test_helper.create_group(alice_group, aliceUID, aliceUID, true));
+
+    await admin_db.doc(`groups/1/members/${aliceUID}`).set({
+      uid: aliceUID,
+      groupId: "1",
+    });
+    await admin_db.doc(`groups/2/members/${aliceUID}`).set({
+      uid: aliceUID,
+      groupId: "2",
+    });
+    await admin_db.doc(`groups/3/members/${aliceUID}`).set({
+      uid: aliceUID,
+      groupId: "3",
+    });
+    await admin_db.doc(`groups/4/members/${aliceUID}`).set({
+      uid: aliceUID,
+      groupId: "4",
+    });
+
+    const topic_ids = ["g_1", "g_2", "g_3", "g_4"];
+    await index.subscribe_topic([123], [], "1", admin_db, (tokens, topic) => {
+      tokens.should.members([123]);
+      topic.should.equal(topic_ids.shift());
+    });
+  })
+})
+    
