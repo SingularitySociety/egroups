14778f59eaaf73fbf4c061ed9c79df21aabdf81b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-15 05:13:10 -0700

    edit member name

--- src/group/Account.js
+++ src/group/Account.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, Button } from '@material-ui/core';
+import { Typography, Button, FormGroup } from '@material-ui/core';
 import { Redirect } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+import EditableField from '../common/EditableField';
 
 const styles = theme => ({
     button: {
@@ -23,15 +24,28 @@ class Account extends React.Component {
         this.props.memberDidUpdate();
         window.location.pathname = "/" + group.groupName;
     }
-
+    onSave = name => async value => {
+        //console.log(name, value);
+        const { db, user, group } = this.props;
+        const refMember = db.doc(`groups/${group.groupId}/members/${user.uid}`);
+        await refMember.set({[name]:value}, {merge:true});
+        //this.props.reloadGroup();
+      }    
     
     render() {
         const { classes, group, user, member } = this.props;
         if (!user) {
             return <Redirect to={`/${group.groupName}`} />
         }
+        if (!member) {
+            return "";
+        }
         console.log(member);
         return <div>
+          <FormGroup row>
+            <EditableField label={<FormattedMessage id="member.displayName"/>} 
+                value={member.displayName} onSave={this.onSave('displayName')}/>
+          </FormGroup>
           <Typography component="h2" variant="h6" gutterBottom>
             <FormattedMessage id="account" />
           </Typography>
