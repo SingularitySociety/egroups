fdd1a82f45647922299c14576bffbb39fbb1b4cf
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-15 08:59:27 -0700

    github

--- src/config.js
+++ src/config.js
@@ -16,6 +16,7 @@ export const appConfig = {
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
     //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
   ],
   //rootGroup: "betatest",
