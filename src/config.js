f81f0bc09cbf0543e6453982e048667f406236c7
Isamu Arimoto <isamu@to-kyo.to>
2019-06-16 19:01:01 +0900

    upadte project env

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjiYcArEzy5_0kc1Bq6uo0pHKrDYpBWKY",
-  authDomain: "e-groups.firebaseapp.com",
-  databaseURL: "https://e-groups.firebaseio.com",
-  projectId: "e-groups",
-  storageBucket: "e-groups.appspot.com",
-  messagingSenderId: "866990675127",
-  appId: "1:866990675127:web:821b9610148c3b2b",
-  messageKey: "BCrvhfMDgC5kuQaNsD1pL1uB_s-LdPsxilBoknX-Vs4u8ULzYCbOw9LBjo9jSLWnK6xgzvhZKFMFz8ymkujEn4I",
+  apiKey: "AIzaSyD-i5bqwVjvmqWeaDIgBbkzH5b79PF3kUE",
+  authDomain: "e-group-test.firebaseapp.com",
+  databaseURL: "https://e-group-test.firebaseio.com",
+  projectId: "e-group-test",
+  storageBucket: "e-group-test.appspot.com",
+  messagingSenderId: "198486292334",
+  appId: "1:198486292334:web:c8a7e6371b3c6b86",
+  messageKey: "BJjXvPfvKS28CdLu4vbnXoajer-DiBlbJLTNsarbXpm9A_ha1HLqnMhXbJB8eR5cSEnXsfpesk0Z9uzlo_DIoV4",
 };
 
 export const appConfig = {
