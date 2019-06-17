942581db0ce67ba1ea8722b1ceba2e111c5b1a65
Isamu Arimoto <isamu@to-kyo.to>
2019-06-17 12:07:23 +0900

    register topic when new group join

--- functions/src/messaging.ts
+++ functions/src/messaging.ts
@@ -1,13 +1,27 @@
 import * as admin from 'firebase-admin';
 import * as utils from './utils'
 
+const groupId_to_topic = (groupId) => {
+  return "g_" + groupId;
+}
 
 const get_topic_from_group = async (db, uid) => {
   const groupsSnapShot = await db.collectionGroup('members').where("uid", "==", uid).get()
   const topics = groupsSnapShot.docs.map((doc) => {
-    return "g_" + doc.data().groupId;
+    return groupId_to_topic(doc.data().groupId);
   });
   return topics;
+}
+
+export const subscribe_new_group = async(userId, groupId, db, subscribe) => {
+  console.log(`/users/${userId}/private/tokens`);
+  const token_data = await db.doc(`/users/${userId}/private/tokens`).get();
+  if (token_data && token_data.data()) {
+    const tokens = token_data.data().tokens;
+    if (tokens && tokens.length > 0) {
+      await subscribe(tokens, groupId_to_topic(groupId))
+    }
+  }
 }
 
 export const subscribe_group = async (newTokens, oldTokens, userId, db, subscribe, unsubscribe) => {
@@ -37,18 +51,18 @@ export const subscribe_group = async (newTokens, oldTokens, userId, db, subscrib
 
 }
 
-export const subscribe_topic = (tokens, topic) => {
+export const subscribe_topic = async (tokens, topic) => {
   try {
-    const response = admin.messaging().subscribeToTopic(tokens, topic);
+    const response = await admin.messaging().subscribeToTopic(tokens, topic);
     console.log('Successfully subscribed to topic:', response);
   } catch(error) {
     console.log('Error subscribing to topic:', error);
   }
 }
 
-export const unsubscribe_topic = (tokens, topic) => {
+export const unsubscribe_topic = async (tokens, topic) => {
   try {
-    const response = admin.messaging().unsubscribeFromTopic(tokens, topic);
+    const response = await admin.messaging().unsubscribeFromTopic(tokens, topic);
     console.log('Successfully subscribed to topic:', response);
   } catch(error) {
     console.log('Error subscribing to topic:', error);
