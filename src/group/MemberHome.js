a8895dc2b5c096a31492b04f8ae7ba1dc6f0f7df
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-18 17:16:35 -0700

    MemberHome didMount

--- src/group/MemberHome.js
+++ src/group/MemberHome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import MountDetector from '../common/MountDetector';
 
 const styles = theme => ({
 });
@@ -11,12 +12,20 @@ class MemberHome extends React.Component {
       const { selectTab } = this.props;
       selectTab("member");
     }
+    memberDidMount = () => {
+      const { member, user } = this.props;
+      console.log("didMout", member, user);
+    }
+    memberWillUnmount = () => {
+    }
     render() {
-        return (
-            <Typography component="h2" variant="h5" gutterBottom>
-              Member-only Page
-            </Typography>
-          )
+      const { member } = this.props;
+      return (<React.Fragment>
+          { member && <MountDetector didMount={this.memberDidMount} willUnmount={this.memberWillUnmount} />}
+          <Typography component="h2" variant="h5" gutterBottom>
+            Member-only Page
+          </Typography>
+        </React.Fragment>)
     }
 }
 
