35e695135076c3ed92044deca9b547ca04bd11ff
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-22 13:53:15 -0700

    Image for MemberItem

--- src/group/MemberItem.js
+++ src/group/MemberItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import MUILink from '@material-ui/core/link';
 import { Link } from 'react-router-dom';
+import ImageUploader from '../common/ImageUploader';
 
 
 const styles = theme => ({
@@ -14,8 +15,11 @@ const styles = theme => ({
 class MemberItem extends React.Component {
   render() {
     const { group, item, classes } = this.props;
+    const imagePath = `groups/${group.groupId}/members/${item.uid}/images/profile`;
     return (
       <MUILink component={Link} to={`/${group.groupName}/pr/${item.uid}`} className={classes.member}>
+        <ImageUploader imagePath={imagePath} loadImage={item.hasImage} 
+                readOnly={true} displayMode={null} onImageUpload={this.onImageUpload} />
         {item.displayName}
       </MUILink>
     )
