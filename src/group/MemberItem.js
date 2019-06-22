01b52c07ad5468cf9fba7d28a7f38e3e3fc81f72
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-22 14:20:02 -0700

    thumbMiddle inline

--- src/group/MemberItem.js
+++ src/group/MemberItem.js
@@ -2,13 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import MUILink from '@material-ui/core/link';
+import { Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import ImageUploader from '../common/ImageUploader';
 
 
 const styles = theme => ({
   member: {
+    marginBottom: theme.spacing(1),
     color: "#333",
+  },
+  name: {
+    marginLeft: theme.spacing(1),
+    paddingTop: "1.5em",
   }
 });
 
@@ -18,9 +24,13 @@ class MemberItem extends React.Component {
     const imagePath = `groups/${group.groupId}/members/${item.uid}/images/profile`;
     return (
       <MUILink component={Link} to={`/${group.groupName}/pr/${item.uid}`} className={classes.member}>
-        <ImageUploader imagePath={imagePath} loadImage={item.hasImage} 
-                readOnly={true} displayMode={null} onImageUpload={this.onImageUpload} />
-        {item.displayName}
+        <Grid container>
+            <ImageUploader imagePath={imagePath} loadImage={item.hasImage} 
+                readOnly={true} displayMode={"thumbMiddle"} inline={true} onImageUpload={this.onImageUpload} />
+            <Grid item class={classes.name}>
+              {item.displayName}
+            </Grid>
+        </Grid>
       </MUILink>
     )
   }
