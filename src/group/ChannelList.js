888e282ba86aa0dcaddfeaf2188d7f394ba8aef8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-22 14:41:15 -0700

    Grid

--- src/group/ChannelList.js
+++ src/group/ChannelList.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import * as firebase from "firebase/app";
-import "firebase/firestore";
-import CreateNew from '../common/CreateNew';
 import ChannelItem from './ChannelItem';
-import { FormattedMessage } from 'react-intl';
-import Privileges from '../const/Privileges';
 
 const styles = theme => ({
 });
@@ -28,24 +23,9 @@ class ChannelList extends React.Component {
   componentWillUnmount() {
     this.detacher();
   }
-  createChannel = async (title) => {
-    console.log("createChannel:", title)
-    const { db, group, user } = this.props;
-    db.collection(`groups/${group.groupId}/channels`).add({
-      title,
-      created: firebase.firestore.FieldValue.serverTimestamp(),
-      owner: user.uid,
-      read: group.privileges.channel.read || Privileges.member, 
-      write: group.privileges.channel.write || Privileges.member, 
-    });
-  }
   render() {
-    const { member, group, history } = this.props;
-    const canCreateNew = !!member && member.privilege 
-            >= ((group.privileges && group.privileges.channel && group.privileges.channel.create) || 2);
+    const { group, history } = this.props;
     return <div>
-      { canCreateNew && <CreateNew createNew={ this.createChannel } 
-          action={<FormattedMessage id="create" />} label={<FormattedMessage id="channel" />}/> }
       <div>
         {
           this.state.list.map((channel)=>{
