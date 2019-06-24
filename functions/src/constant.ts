a58193ee8d1f1dba644e0474b59da662a5adbb82
Isamu Arimoto <isamu@to-kyo.to>
2019-06-24 10:56:21 +0900

    support thumbnails

--- functions/src/constant.ts
+++ functions/src/constant.ts
@@ -1,13 +1,15 @@
+// groups/${group.groupId}/articles/${article.articleId}/${sectionId}  article
+// groups/${group.groupId}/members/${item.uid}/images/profile          member
+// groups/${group.groupId}/images/profile                              group
+
 export const thumbnailSizes = [600, 1200];
 
 export const articlePath = {path: {0: "groups", 2: "articles"}, length: 5};
-export const imagePath = {path: {0: "groups", 2: "images"}, length: 5}; // ??
-export const groupProfilePath = {path: {0: "groups", 2: "profile"}, length: 3};
 export const memberPath = {path: {0: "groups", 2: "members", 4: "images"}, length: 6};
+export const groupProfilePath = {path: {0: "groups", 2: "images"}, length: 4};
 
 export const matchImagePaths = [
   articlePath,
-  imagePath,
-  groupProfilePath,
   memberPath,
+  groupProfilePath,
 ];
