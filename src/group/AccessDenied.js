a19e1c066f6f666104258214acee64c7f260db1d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-26 15:25:39 -0700

    more hooks

--- src/group/AccessDenied.js
+++ src/group/AccessDenied.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+//import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import { FormattedMessage } from 'react-intl';
 
@@ -10,19 +10,18 @@ const styles = theme => ({
   }
 });
 
-class AccessDenied extends React.Component {
-  render() {
-    const {classes} = this.props;
-    return (
-      <Typography component="h2" variant="h5" gutterBottom　className={ classes.errorMessage} >
-        <FormattedMessage id="warning.access.denied" />
-      </Typography>
-    )
-  }
+const useStyles = makeStyles(styles);
+
+function AccessDenied() {
+  const classes = useStyles();
+  return (
+    <Typography component="h2" variant="h5" gutterBottom　className={ classes.errorMessage} >
+      <FormattedMessage id="warning.access.denied" />
+    </Typography>
+  )
 }
 
 AccessDenied.propTypes = {
-    classes: PropTypes.object.isRequired,
   };
   
-export default withStyles(styles)(AccessDenied);
+export default AccessDenied;
