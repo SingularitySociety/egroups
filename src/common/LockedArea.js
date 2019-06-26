4f72166d66cacbdd55b4fb5e5e1f3264a7a25e63
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-06-25 22:05:40 -0700

    use hooks

--- src/common/LockedArea.js
+++ src/common/LockedArea.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import LockIcon from '@material-ui/icons/Lock';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   button: {
     marginRight: theme.spacing(1),
   },
@@ -15,33 +14,23 @@ const styles = theme => ({
   unlocked: {
     padding: theme.spacing(1),
   }
-});
+}));
 
-class LockedArea extends React.Component {
-  state = { locked: true }
-  toggle = () => {
-    const locked = !this.state.locked;
-    this.setState({locked});
-  }
-  render() {
-    const { classes, children, label } = this.props;
-    const { locked } = this.state;
-    return (
-      <div>
-      <IconButton onClick={this.toggle} className={classes.button}>
+export default function LockedArea(props) {
+  const classes = useStyles();
+  const [locked, setLocked] = useState(true);
+  const { children, label } = props;
+
+  return (
+    <div>
+      <IconButton onClick={()=>{setLocked(!locked)}} className={classes.button}>
         { locked ? <LockIcon/> : <LockOpenIcon/> }
       </IconButton>
-      { label }
+        { label }
       <div className={classes[locked ? "locked" : "unlocked"]}>
         { children }
       </div>
     </div>
-    )
-  }
+  )
 }
 
-LockedArea.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
-  
-export default withStyles(styles)(LockedArea);
