77b1be695ac40c0aaaa09b0457ea8e6fcc9a9e8d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-02 22:14:57 -0700

    hooks

--- src/GroupList.js
+++ src/GroupList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Paper, Grid } from '@material-ui/core';
@@ -15,40 +15,40 @@ const styles = theme => ({
   },
 });
 
-class GroupList extends React.Component {
-  state = { groups:[] };
-  async componentDidMount() {
-    const { db } = this.props;
-    const ref = db.collection("groups").where("groupName", ">", "");
-    const snapshot = await ref.get();
-    //console.log(snapshot);
-    const groups = [];
-    snapshot.forEach((doc)=>{
-        const group = doc.data();
-        group.groupId = doc.id;
-        groups.push(group);
-    });
-    this.setState({groups:groups});
-  }
+function GroupList(props) {
+  const [groups, setGroups] = useState([]);
+  const { db, classes } = props;
+  useEffect(()=>{
+    async function fetchList() {
+      const ref = db.collection("groups").where("groupName", ">", "");
+      const snapshot = await ref.get();
+      //console.log(snapshot);
+      const groups = [];
+      snapshot.forEach((doc)=>{
+          const group = doc.data();
+          group.groupId = doc.id;
+          groups.push(group);
+      });
+      setGroups(groups);
+    } 
+    fetchList();   
+  }, [db]);
 
-  render() {
-    const { classes } = this.props;
-    return <Grid container justify="center">
-        { 
-            this.state.groups.map((group)=> {
-                return (
-                  <Grid item key={group.groupId}  xs={12}>
-                  <MUILink component={Link} className={classes.link}
-                    to={"/" + (group.groupName || group.groupId)}>
-                      <Paper className={classes.paper}> 
-                    {group.title}
-                    </Paper>
-                  </MUILink>
-                  </Grid>);
-            })
-        }
-    </Grid>
-  }
+  return <Grid container justify="center">
+    { 
+      groups.map((group)=> {
+          return (
+            <Grid item key={group.groupId}  xs={12}>
+            <MUILink component={Link} className={classes.link}
+              to={"/" + (group.groupName || group.groupId)}>
+                <Paper className={classes.paper}> 
+              {group.title}
+              </Paper>
+            </MUILink>
+            </Grid>);
+      })
+    }
+  </Grid>
 }
 
 GroupList.propTypes = {
