230d6930d4e77d6bc0e732da171afc2ebf386d78
Isamu Arimoto <isamu@to-kyo.to>
2019-07-10 08:52:20 +0900

    fix test

--- functions/test/group_function_test.ts
+++ functions/test/group_function_test.ts
@@ -50,7 +50,7 @@ describe('Group function test', () => {
   it ('update test', async function() {
     this.timeout(10000);
 
-    const groupId = "123"
+    const groupId = "123_" + UUID();
 
     const uuid = UUID();
     const aliceUserId = "test_customer_" + uuid;
@@ -90,22 +90,26 @@ describe('Group function test', () => {
     };
     await wrapped(change, context);
 
-    await wrapped(change, context);
-
     const data = (await admin_db.doc(`/groups/${groupId}/secret/stripe`).get()).data()
+
+    const productionId = stripeUtils.getProductId(groupId);
+    const plan2000Id = stripeUtils.getPlanId(groupId, 2000, "jpy");
+    const plan3000Id = stripeUtils.getPlanId(groupId, 3000, "jpy");
+    const plan30Id = stripeUtils.getPlanId(groupId, 30, "usd");
     
     const planData = data.plans;
+
     const planData2000 = planData["2000_jpy"];
     planData2000.active.should.equal(true);
     planData2000.amount.should.equal(2000);
     planData2000.billing_scheme.should.equal('per_unit');
     planData2000.currency.should.equal('jpy');
-    planData2000.id.should.equal('plan_123_2000_jpy');
+    planData2000.id.should.equal(plan2000Id);
     planData2000.interval.should.equal('month');
     planData2000.interval_count.should.equal(1);
     planData2000.livemode.should.equal(false);
     planData2000.object.should.equal('plan');
-    planData2000.product.should.equal('prod_123');
+    planData2000.product.should.equal(productionId);
     planData2000.usage_type.should.equal('licensed');
       
     const planData3000 = planData["3000_jpy"];
@@ -113,19 +117,19 @@ describe('Group function test', () => {
     planData3000.amount.should.equal(3000);
     planData3000.billing_scheme.should.equal('per_unit');
     planData3000.currency.should.equal('jpy');
-    planData3000.id.should.equal('plan_123_3000_jpy');
+    planData3000.id.should.equal(plan3000Id);
     planData3000.interval.should.equal('month');
     planData3000.interval_count.should.equal(1);
     planData3000.livemode.should.equal(false);
     planData3000.object.should.equal('plan');
-    planData3000.product.should.equal('prod_123');
+    planData3000.product.should.equal(productionId);
     planData3000.usage_type.should.equal('licensed');
 
     const production = data.production;
     production.active.should.equal(true);
-    production.id.should.equal('prod_123');
+    production.id.should.equal(productionId);
     production.livemode.should.equal(false);
-    production.metadata.should.deep.equal({ groupId: '123' });
+    production.metadata.should.deep.equal({ groupId: groupId });
     production.name.should.equal('hello');
     production.object.should.equal('product');
     production.statement_descriptor.should.equal('hello');
@@ -136,28 +140,30 @@ describe('Group function test', () => {
                               [ { active: true,
                                   amount: 2000,
                                   currency: 'jpy',
-                                  id: 'plan_123_2000_jpy',
+                                  id: plan2000Id,
                                   interval: 'month',
                                   interval_count: 1 },
                                 { active: true,
                                   amount: 3000,
                                   currency: 'jpy',
-                                  id: 'plan_123_3000_jpy',
+                                  id: plan3000Id,
                                   interval: 'month',
                                   interval_count: 1 },
                                 { active: true,
                                   amount: 30,
                                   currency: 'usd',
-                                  id: 'plan_123_30_usd',
+                                  id: plan30Id,
                                   interval: 'month',
                                   interval_count: 1 } ],
                               production:
                               { active: true,
-                                id: 'prod_123',
+                                id: productionId,
                                 name: 'hello',
                                 statement_descriptor: 'hello',
                                 type: 'service' } });
 
+    await wrapped(change, context);
+    
   });
 
   it ('stripe create customer test', async function() {
