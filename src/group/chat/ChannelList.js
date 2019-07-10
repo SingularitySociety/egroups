39412caab2af8ffbe97e6fd2e9f86670ec56c91e
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-10 09:23:20 -0700

    more hooks

--- src/group/chat/ChannelList.js
+++ src/group/chat/ChannelList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import ChannelItem from './ChannelItem';
@@ -7,47 +7,42 @@ import ErrorMessage from '../ErrorMessage';
 const styles = theme => ({
 });
 
-class ChannelList extends React.Component {
-  state = { list:[] }
-  componentDidMount() {
-    const { db, group, limit } = this.props;
+function ChannelList(props) {
+  const { db, group, limit, history } = props;
+  const [list, setList] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
     let query = db.collection(`groups/${group.groupId}/channels`).orderBy("created", "desc");
     if (limit) {
       query = query.limit(limit);
     }
-    this.detacher = query.onSnapshot((snapshot) => {
+    const detacher = query.onSnapshot((snapshot) => {
       const list = [];
       snapshot.forEach((doc)=>{
         const channel = doc.data();
         channel.channelId = doc.id;
         list.push(channel);
       });
-      this.setState({list});
+      setList(list);
     }, (e) => {
       console.log(e);
       const error = { key: "warning.access.denied" };
-      this.setState({error});
+      setError(error);
     })
+    return detacher;
+  }, [db, group, limit]);
+
+  if (error) {
+    return <ErrorMessage error={error} />
   }
-  componentWillUnmount() {
-    this.detacher();
-  }
-  render() {
-    const { group, history } = this.props;
-    const { error } = this.state;
-    if (error) {
-      return <ErrorMessage error={error} />
+  return <div>
+    {
+      list.map((channel)=>{
+        return <ChannelItem key={channel.channelId} channel={channel} group={group} history={history} />
+      })
     }
-    return <div>
-      <div>
-        {
-          this.state.list.map((channel)=>{
-            return <ChannelItem key={channel.channelId} channel={channel} group={group} history={history} />
-          })
-        }
-      </div>
-    </div>
-  }
+  </div>
 }
 
 ChannelList.propTypes = {
