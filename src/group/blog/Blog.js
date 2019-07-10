4532410413d59bdec03abd5fbe0e95efbb2f9d4a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-10 15:20:41 -0700

    more hooks

--- src/group/blog/Blog.js
+++ src/group/blog/Blog.js
@@ -1,62 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import ErrorMessage from '../ErrorMessage';
 import BlogArticle from './BlogArticle';
+import useDocument from '../../common/useDocument';
 
 const styles = theme => ({
 });
 
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    const { group, arp, match:{params:{articleId}} } = this.props;
-    this.pathArticle = `groups/${group.groupId}/${arp.collection}/${articleId}`;
-    this.state = {article:null, sections:[], resouces:null};
-  }
+function Blog(props) {
+  const { match:{params:{articleId}}, callbacks, arp, user, db, group } = props;
+  const { privilege, profiles } = props;
+  const pathArticle = `groups/${group.groupId}/${arp.collection}/${articleId}`;
+  const [ article, err ] = useDocument(pathArticle);
+  const setTabbar = callbacks.setTabbar;
 
-  async componentDidMount() {
-    const { match:{params:{articleId}}, callbacks, arp, user, db, group } = this.props;
-    callbacks.setTabbar(arp.tabLeaf, `${arp.leaf}/${articleId}`);
+  useEffect(()=>{
+    setTabbar(arp.tabLeaf, `${arp.leaf}/${articleId}`);
+  }, [setTabbar, arp, articleId]);
 
-    const error = {key:"error.invalid.articleId", value:articleId};
-    try {
-      const refArticle = db.doc(this.pathArticle);
-      const article = (await refArticle.get()).data();
-      if (article) {
-        article.articleId = articleId;
-        this.setState({article});
-
-        if (user) {
-          const articles = {};
-          articles[articleId] = { l:new Date() }; // NOT firebase.firestore.FieldValue.serverTimestamp()
-          db.doc(`groups/${group.groupId}/members/${user.uid}/private/history`).set({
-            articles
-          }, {merge:true})
-        }
-
-        return;
-      }
-    } catch(e) {
-      console.log(e);
-      // LATER: Detect network error case
-      error.key = "warning.access.denied";
+  useEffect(()=>{
+    if (user) {
+      const articles = {};
+      articles[articleId] = { l:new Date() }; // NOT firebase.firestore.FieldValue.serverTimestamp()
+      db.doc(`groups/${group.groupId}/members/${user.uid}/private/history`).set({
+        articles
+      }, {merge:true})
     }
-    this.setState({ error });
-  }
+  },[db, user, articleId, group.groupId, user.id]);
 
-  render() {
-    const { article, error } = this.state;
-    const { user, arp, group, privilege, db, profiles, callbacks } = this.props;
-    const context = { user, article, arp, group, privilege, db, profiles, callbacks };
-    if (error) {
-      return <ErrorMessage error={error} />
-    }
-    if (!article) {
-      return "";
-    }
-    return <BlogArticle {...context} pathArticle={this.pathArticle}/>;
+  if (err) {
+    return <ErrorMessage error={{key:"error.invalid.articleId", value:articleId}} />
   }
+  
+  if (!article) {
+    return "";
+  }
+  article.articleId = articleId;
+  const context = { user, article, arp, group, privilege, db, profiles, callbacks };
+  return <BlogArticle {...context} pathArticle={this.pathArticle}/>;
 }
 
 Blog.propTypes = {
