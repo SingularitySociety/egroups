155a48b12d438ec853216d4c4580a9b43da9f669
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-10 21:59:48 -0700

    more hooks

--- src/common/ImageUploader.js
+++ src/common/ImageUploader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Grid, IconButton } from '@material-ui/core';
@@ -47,32 +47,40 @@ const styles = theme => ({
   },
 });
 
-class ImageViewer extends React.Component {
-  state = {imageUrl:null};
-  async componentDidMount() {
-    const { imagePath, loadImage, imageUrl, imageThumbnails, displayMode } = this.props;
+function ImageViewer(props) {
+  const { imagePath, loadImage, imageUrl, imageThumbnails, displayMode, onImageUpload } = props;
+  const { classes, readOnly, inline, deleteImage } = props;
+  const [ url, setUrl ] = useState(null);
+
+  useEffect(() => {
     //console.log(imageUrl, imagePath, imageThumbnails);
     const storagRef = firebase.storage().ref();
-    this.imageRef = storagRef.child(imagePath);
+    const imageRef = storagRef.child(imagePath);
     const thumbnailSize = (displayMode === "wide") ? 1200 : 600;
     
     if (imageThumbnails && imageThumbnails[thumbnailSize]) {
       console.log("Use thumbnail");
-      this.setState({imageUrl: imageThumbnails[thumbnailSize]});
+      setUrl(imageThumbnails[thumbnailSize]);
       return;
     }
     if (imageUrl) {
-      this.setState({imageUrl});
+      setUrl(imageUrl);
       return;
     }
     if (loadImage) {
       console.log(imagePath);
-      const imageUrl = await this.imageRef.getDownloadURL();
-      this.setState({imageUrl});
+      async function getDownloadUrl() {
+        const downloadUrl = await imageRef.getDownloadURL();
+        setUrl(downloadUrl);
+      }
+      getDownloadUrl();
     }
-  }
-  onFileInput = (e) => {
-    const task = this.imageRef.put(e.target.files[0]);
+  }, [imagePath, displayMode, imageThumbnails, imageUrl, loadImage]);
+
+  const onFileInput = (e) => {
+    const storagRef = firebase.storage().ref();
+    const imageRef = storagRef.child(imagePath);
+    const task = imageRef.put(e.target.files[0]);
     task.on('state_changed', (snapshot)=>{
       console.log("progress", snapshot);
     }, (error) => {
@@ -81,41 +89,38 @@ class ImageViewer extends React.Component {
       // Accordign to the document, this download URL is valid until you explicitly reveke it,
       // and it is accessible by anybody (no security). The security is at this getDownloadURL() level. 
       // It is fine to cache this URL in the database because we put a security around the database.
-      const imageUrl = await task.snapshot.ref.getDownloadURL();
-      this.props.onImageUpload(imageUrl);
-      this.setState({imageUrl});
-    })
+      const downloadUrl = await task.snapshot.ref.getDownloadURL();
+      onImageUpload(downloadUrl);
+      setUrl(downloadUrl);
+    });
   }    
-  render() {
-    const { classes, readOnly, displayMode, inline, onImageUpload, deleteImage } = this.props;
-    const { imageUrl } = this.state;
-    const imageStyle = imageUrl ? { backgroundImage:`url("${imageUrl}")` } : {};
-    const imageElement = (displayMode === "wide") ? (
-        <Grid item xs={readOnly ? 12 : 11} className={ classes.wideFrame }>
-          <img src={imageUrl} className={classes[displayMode]} alt="blog article" />
-        </Grid>
-      ) : <Grid item className={classes[displayMode || "thumbLarge"]} style={imageStyle} />;
-    if (inline) {
-      return imageElement;
-    }
-    return (<Grid container className={classes.root} spacing={1} justify="center">
-        { imageElement }
-        {
-          !readOnly && onImageUpload &&
-            <Grid item xs={1}>
-            <IconButton size="small" variant="contained" component="label">
-                <UploadButton />
-                <input type="file" accept="image/*" style={{ display: "none" }} onChange={this.onFileInput} />
-            </IconButton>
-            {
-              deleteImage && 
-              <IconButton size="small" onClick={deleteImage}><DeleteIcon /></IconButton>
-          }
-        </Grid>
 
-        }
-      </Grid>)
+  const imageStyle = url ? { backgroundImage:`url("${url}")` } : {};
+  const imageElement = (displayMode === "wide") ? (
+      <Grid item xs={readOnly ? 12 : 11} className={ classes.wideFrame }>
+        <img src={url} className={classes[displayMode]} alt="blog article" />
+      </Grid>
+    ) : <Grid item className={classes[displayMode || "thumbLarge"]} style={imageStyle} />;
+  if (inline) {
+    return imageElement;
   }
+  return (<Grid container className={classes.root} spacing={1} justify="center">
+      { imageElement }
+      {
+        !readOnly && onImageUpload &&
+          <Grid item xs={1}>
+          <IconButton size="small" variant="contained" component="label">
+              <UploadButton />
+              <input type="file" accept="image/*" style={{ display: "none" }} onChange={onFileInput} />
+          </IconButton>
+          {
+            deleteImage && 
+            <IconButton size="small" onClick={deleteImage}><DeleteIcon /></IconButton>
+        }
+      </Grid>
+
+      }
+    </Grid>)
 }
 
 ImageViewer.propTypes = {
