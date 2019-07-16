6397b57ad083a3caa45d21141a6823572b173dff
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-16 14:59:20 -0700

    Invite menu

--- src/group/join/Invite.js
+++ src/group/join/Invite.js
@@ -1,19 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { FormControl, InputLabel, Select, Button } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
+import PrivilegeOptions from '../PrivilegeOptions';
 
 const styles = theme => ({
+  formControl: {
+    width:theme.spacing(38),
+    marginBottom: theme.spacing(2),
+  },
 });
 
 function Invite(props) {
-  const { callbacks } = props;
+  const { callbacks, classes } = props;
   const setTabbar = callbacks.setTabbar;
+  const [level, setLevel] = useState(0);
 
   useEffect(()=>{
     setTabbar("invite");
   }, [setTabbar]);
 
-  return "Invite"
+  function handleLevel(e) {
+    setLevel(e.target.value);
+  }
+
+  function handleInvite(e) {
+    console.log("invite");
+  }
+
+  return <React.Fragment>
+    <FormControl className={classes.formControl}>
+      <InputLabel><FormattedMessage id="invitation.privilege" /></InputLabel>
+      <Select　native　value={level}　onChange={handleLevel}>
+        <PrivilegeOptions />
+      </Select>
+    </FormControl>
+    <br/>
+    <Button variant="contained" onClick={handleInvite}>
+      <FormattedMessage id="invite" />
+    </Button>
+  </React.Fragment>
 }
 
 Invite.propTypes = {
