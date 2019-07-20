1fa4e9f3962a713b35a49fd68b56b25de3dcaf55
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-20 10:44:45 -0700

    avoid CORS policy error

--- functions/src/functions/email.ts
+++ functions/src/functions/email.ts
@@ -1,6 +1,17 @@
 import * as utils from '../utils/utils';
+//const fs = require('fs');
+//const util = require('util');
+
+//const readFile = util.promisify(fs.readFile);
 
 export const sendMail = async (db, data, context) => {
-  await utils.sendMail("satoshi.nakajima@gmail.com", "test", "body", "body");
-  return { return:true };
+  try {
+    //const { err, body } = readFile('../../templates/foo.txt');
+    const err = "foo";
+    const body = "bar";
+    await utils.sendMail("satoshi.nakajima@gmail.com", "test", body, body);
+    return { return:true, err };
+  } catch(e) {
+    return { return:false, e };
+  }
 };
