3903bee04601c29ba3746cc868a1fd4216ad07e8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-22 14:49:21 -0700

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
@@ -98,84 +98,78 @@ function getPushToken(user) {
   }
 }
 
-class App extends React.Component {
-  state = {};
-  
-  componentDidMount() {
-    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-      async (user) => {
-        if (this.detachPrivilegesObserver) {
-          this.detachPrivilegesObserver();
-          this.detachPrivilegesObserver = null;
-        }
-        this.setState({user: user});
-        if (user) {
-          this.detachPrivilegesObserver = db.doc(`privileges/${user.uid}`).onSnapshot(async (snapshot) => {
-            const getJWT = firebase.functions().httpsCallable('getJWT');
-            const token = (await getJWT()).data; 
-            //console.log("token", token);
-            try {
-              await firebase.auth().signInWithCustomToken(token.token);
-            } catch(e) {
-              console.log(e);
-            }
-            console.log({privileges:token.privileges});
-            this.setState({privileges: token.privileges});
-          });
+function App() {
+  const [user, setUser] = useState(null);
+  const [privileges, setPrivileges] = useState(null);
 
-          const refUser = db.collection("users").doc(user.uid);
-          const newValue = { lastAccessed:firebase.firestore.FieldValue.serverTimestamp() };
-          const doc = (await refUser.get()).data();
-          if (!doc || !doc.displayName) {
-            newValue.displayName = user.displayName;
-          }
-          await refUser.set(newValue, { merge: true });
-          getPushToken(user);
-        }
+  useEffect(()=>{
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+      async (user) => {
+        setUser(user);
       }
     );
-    
-  }
+    return unregisterAuthObserver;
+  }, []);
 
+  useEffect(()=>{
+    if (user) {
+      const detachPrivilegesObserver = db.doc(`privileges/${user.uid}`).onSnapshot(async (snapshot) => {
+        const getJWT = firebase.functions().httpsCallable('getJWT');
+        const token = (await getJWT()).data; 
+        //console.log("token", token);
+        try {
+          await firebase.auth().signInWithCustomToken(token.token);
+        } catch(e) {
+          console.log(e);
+        }
+        console.log({privileges:token.privileges});
+        setPrivileges(token.privileges);
+      });
 
-  componentWillUnmount() {
-    if (this.detachPrivilegesObserver) {
-      this.detachPrivilegesObserver();
+      async function updateUser() {
+        const refUser = db.collection("users").doc(user.uid);
+        const newValue = { lastAccessed:firebase.firestore.FieldValue.serverTimestamp() };
+        const doc = (await refUser.get()).data();
+        if (!doc || !doc.displayName) {
+          newValue.displayName = user.displayName;
+        }
+        await refUser.set(newValue, { merge: true });
+        console.log("### calling getPushToken")
+        getPushToken(user);
+      }
+      updateUser();
+      return detachPrivilegesObserver;
     }
-    this.unregisterAuthObserver();
-  }
-  
-  render() {
-    const { user, privileges } = this.state;
-    const params = { user, db, privileges, rootGroup:appConfig.rootGroup };
-    //console.log("App:", window.location.pathname);
-    const language = navigator.language.split(/[-_]/)[0];  // language without region code
-    return (
-      <IntlProvider locale={language} messages={messages[language]}>
-        <MuiThemeProvider theme={theme}>
-          <CssBaseline />
-          <Router>
-            {
-              appConfig.rootGroup ?
-              <Route exact path="/" render={(props) => <GroupRouter {...props} {...params} joinGroup={this.joinGroup} />} />
-              : <Route exact path="/" render={(props) => <Home {...props} {...params} />} />
-            }
-            <Route path="/:gp" render={(props) => <GroupRouter {...props} {...params} joinGroup={this.joinGroup} />} />
-            <Route exact path="/a/about" render={(props) => <About {...props} {...params} />} />
-            <Route exact path="/a/new/:groupId" render={(props) => <NewGroup {...props} {...params} />} />
-            <Route exact path="/a/login" render={(props) => <Login {...props} {...params} />} />
-            <Route exact path="/a/login/cmd/:encoded" render={(props) => <Login {...props} {...params} />} />
-            <Route exact path="/a/login/target/:target" render={(props) => <Login {...props} {...params} />} />
-            { // We need to mount the Decoder component only after the user info became available.
-              (this.state.user) ?
-                <Route exact path="/a/decode/:encoded" render={(props) => <Decoder {...props} {...params} />} />
-                : "" 
-            }
-          </Router>
-        </MuiThemeProvider>
-      </IntlProvider>
-    );
-  }
+  }, [user]);
+
+  const params = { user, db, privileges, rootGroup:appConfig.rootGroup };
+  //console.log("App:", window.location.pathname);
+  const language = navigator.language.split(/[-_]/)[0];  // language without region code
+  return (
+    <IntlProvider locale={language} messages={messages[language]}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          {
+            appConfig.rootGroup ?
+            <Route exact path="/" render={(props) => <GroupRouter {...props} {...params} />} />
+            : <Route exact path="/" render={(props) => <Home {...props} {...params} />} />
+          }
+          <Route path="/:gp" render={(props) => <GroupRouter {...props} {...params} />} />
+          <Route exact path="/a/about" render={(props) => <About {...props} {...params} />} />
+          <Route exact path="/a/new/:groupId" render={(props) => <NewGroup {...props} {...params} />} />
+          <Route exact path="/a/login" render={(props) => <Login {...props} {...params} />} />
+          <Route exact path="/a/login/cmd/:encoded" render={(props) => <Login {...props} {...params} />} />
+          <Route exact path="/a/login/target/:target" render={(props) => <Login {...props} {...params} />} />
+          { // We need to mount the Decoder component only after the user info became available.
+            (user) ?
+              <Route exact path="/a/decode/:encoded" render={(props) => <Decoder {...props} {...params} />} />
+              : "" 
+          }
+        </Router>
+      </MuiThemeProvider>
+    </IntlProvider>
+  );
 }
 
 export default App;
