2d8608e5e724f5ce7fe26af47dc232e63d226ade
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-29 06:34:34 -0700

    gender

---
+++ src/options/GenderOptions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { injectIntl } from 'react-intl';
+
+const Genders = ["female", "male"];
+
+function GenderOptions(props) {
+  const { messages } = props.intl;
+  return <React.Fragment>
+    {
+      Genders.map((gender)=> {
+        return <option key={gender} value={gender}>{messages[gender]}</option>
+      })
+    }
+  </React.Fragment>;
+} 
+
+export default injectIntl(GenderOptions);
