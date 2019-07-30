717af6111c5f8e711d34935353313c29b9c336de
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2019-07-30 12:43:53 -0700

    rouoting number, etc.

--- src/group/join/AccountBankJP.js
+++ src/group/join/AccountBankJP.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { TextField, FormControl } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
 
 const styles = theme => ({
     form: {
@@ -11,19 +13,28 @@ const styles = theme => ({
   });
 
 function AccountBankJP(props) {
-  const { bank_data, setBankData } = props;
-  function setBankValue(key, e) {
+  const { bank_data, setBankData, classes } = props;
+  function setBankValue(key, value) {
     const new_data = (() => {
       if (bank_data) {
         return Object.assign({}, bank_data);
       } 
-      return { country:"JP", currenty:"jpy" };
+      return { object:"bank_account", country:"JP", currency:"jpy" };
     })();
-    new_data[key] = e.target.value;
+    new_data[key] = value;
     setBankData(new_data);
   }
 
-  return <p>"bank"</p>;
+  return (<React.Fragment>
+    {
+      ["routing_number", "account_number", "account_holder_name"].map((key)=>{
+        return <FormControl key={key} className={classes.form}>
+        <TextField error={true} label={<FormattedMessage id={key} />} 
+              value={(bank_data && bank_data[key]) || ""} 
+              onChange={(e)=>setBankValue(key, e.target.value)} />
+      </FormControl>
+      })      
+    }</React.Fragment>);
 }
 
 AccountBankJP.propTypes = {
