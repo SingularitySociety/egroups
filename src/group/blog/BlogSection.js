af443d0612830b1d40a2315f016f90091b80dbdb
Isamu Arimoto <isamu@to-kyo.to>
2019-12-19 18:26:39 +0900

    modify blog editor

--- src/group/blog/BlogSection.js
+++ src/group/blog/BlogSection.js
@@ -1,103 +1,81 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { IconButton, Grid } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import PhotoIcon from '@material-ui/icons/AddPhotoAlternate';
 import EditIcon from '@material-ui/icons/Edit';
 import VideoIcon from '@material-ui/icons/VideoLibrary';
-// import { MarkdownEditor, MarkdownViewer } from 'draft-material.ui-editor';
 import MarkdownViewer from '../../common/packaged/MarkdownViewer';
 import MarkdownEditor from '../../common/packaged/MarkdownEditor';
 import ImageUploader from '../../common/ImageUploader';
 import VideoEditor from '../../common/VideoEditor';
 
 function BlogSection(props) {
-  const [editing, setEditing] = useState(false);
   const { sectionId, index, resource, readOnly, pathArticle } = props;
-
-  function onSave(markdown, raw) {
+  const { editingFlags, updateEditingFlag } = props;
+  function onMarkdownSave(markdown, raw) {
     props.saveSection(sectionId, index, markdown, raw);
-    setEditing(false);
+    updateEditingFlag(sectionId, false);
   }
   function onCancel() {
-    setEditing(false);
+    updateEditingFlag(sectionId, false);
   }
   function onDelete() {
     props.deleteSection(sectionId, index);
   }
-  function startEditing() {
-    setEditing(true);
-  }
-  function insertImage() {
-    props.insertImage(index);
+  function setEditing(flag) {
+    updateEditingFlag(sectionId, flag);
   }
   function onImageUpload(imageUrl) {
     props.onImageUpload(sectionId, imageUrl);
   }
-  function insertVideo() {
-    props.insertVideo(index);
-  }
   function onVideoUpload(videoUrl) {
     props.onVideoUpload(sectionId, videoUrl);
   }
-
-  if (editing) {
+  const editing = (editingFlags||{})[sectionId];
+  
+  if (resource.type==="image") {
+    if (readOnly && !resource.hasImage) {
+      return "";
+    }
+    const imagePath = `${pathArticle}/${sectionId}`;
+    const thumbnails = (resource[sectionId] && resource[sectionId].thumbnails) || resource.thumbnails;
     return (
-      <MarkdownEditor resource={resource} onSave={onSave} onCancel={onCancel} onDelete={props.deleteSection && onDelete} />
+      <ImageUploader imagePath={imagePath} loadImage={resource.hasImage} imageUrl={resource.imageUrl}
+                     imageThumbnails={thumbnails}
+                     readOnly={readOnly} displayMode="wide" onImageUpload={onImageUpload} deleteImage={onDelete} />
     );
   }
+  if (resource.type==="video") {
+    return (
+      <VideoEditor videoUrl={resource.videoUrl} readOnly={readOnly} sectionId={sectionId}
+                   editing={editing} setEditing={setEditing}
+                   displayMode="wide" onVideoUpload={onVideoUpload} deleteVideo={onDelete} />
+    );
+  }
+  if (resource.type==="markdown") {
+    if (editing) {
+      return (
+        <MarkdownEditor resource={resource} onSave={onMarkdownSave} onCancel={onCancel}
+                        onDelete={props.deleteSection && onDelete} />
+      );
+    }
 
-  if (sectionId) {
-    if (resource.type==="image") {
-      if (readOnly && !resource.hasImage) {
-        return "";
-      }
-      const imagePath = `${pathArticle}/${sectionId}`;
-      const thumbnails = (resource[sectionId] && resource[sectionId].thumbnails) || resource.thumbnails;
-      return (
-          <ImageUploader imagePath={imagePath} loadImage={resource.hasImage} imageUrl={resource.imageUrl}
-            imageThumbnails={thumbnails} 
-            readOnly={readOnly} displayMode="wide" onImageUpload={onImageUpload} deleteImage={onDelete} />
-      );
-    }
-    if (resource.type==="video") {
-      return (
-        <VideoEditor videoUrl={resource.videoUrl} readOnly={readOnly} sectionId={sectionId}
-                     displayMode="wide" onVideoUpload={onVideoUpload} deleteVideo={onDelete} />
-      );
-    }
     const textWidth = readOnly ? 12 : 11;
     return <Grid container justify="center">
-      <Grid item xs={textWidth} style={{padding:"1px"}}>
-        <MarkdownViewer resource={resource} />
-      </Grid>
-      { !readOnly &&
-        <Grid item xs={1}>
-          <IconButton size="small" variant="contained" onClick={startEditing}>
-            <EditIcon />
-          </IconButton>
-        </Grid> 
-      }
-    </Grid>;
+             <Grid item xs={textWidth} style={{padding:"1px"}}>
+               <MarkdownViewer resource={resource} />
+             </Grid>
+             { !readOnly &&
+               <Grid item xs={1}>
+                 <IconButton size="small" variant="contained" onClick={(e) => {setEditing(true);}}>
+                   <EditIcon />
+                 </IconButton>
+               </Grid> 
+             }
+           </Grid>;
   }
-
-  return <Grid container justify="center">
-    <Grid item>
-      <IconButton  size="small" variant="contained" onClick={startEditing}>
-        <AddIcon />
-      </IconButton>
-    </Grid> 
-    <Grid item>
-      <IconButton  size="small" variant="contained" onClick={insertImage}>
-        <PhotoIcon />
-      </IconButton>
-    </Grid> 
-    <Grid item>
-      <IconButton  size="small" variant="contained" onClick={insertVideo}>
-        <VideoIcon />
-      </IconButton>
-    </Grid> 
-  </Grid>;
+  return <div/>;
 }
 
 BlogSection.propTypes = {
