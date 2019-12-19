2107e0ae1ce5c79b0defd7a139043615028d2765
Isamu Arimoto <isamu@to-kyo.to>
2019-12-19 19:52:33 +0900

    separate BlogSectionMarkdown

--- src/group/blog/BlogSection.js
+++ src/group/blog/BlogSection.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IconButton, Grid } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import PhotoIcon from '@material-ui/icons/AddPhotoAlternate';
-import EditIcon from '@material-ui/icons/Edit';
-import VideoIcon from '@material-ui/icons/VideoLibrary';
-import MarkdownViewer from '../../common/packaged/MarkdownViewer';
-import MarkdownEditor from '../../common/packaged/MarkdownEditor';
 import ImageUploader from '../../common/ImageUploader';
 import VideoEditor from '../../common/VideoEditor';
+import BlogSectionMarkdown from './BlogSectionMarkdown';
 
 function BlogSection(props) {
   const { sectionId, index, resource, readOnly, pathArticle } = props;
   const { editing, updateEditingFlag } = props;
-  function onSaveMarkdown(markdown, raw) {
-    props.saveMarkdown(sectionId, index, markdown, raw);
-    updateEditingFlag(sectionId, false);
-  }
-  function onCancel() {
-    updateEditingFlag(sectionId, false);
-  }
+
   function onDelete() {
     props.deleteSection(sectionId, index);
   }
@@ -53,33 +41,13 @@ function BlogSection(props) {
     );
   }
   if (resource.type==="markdown") {
-    if (editing) {
-      return (
-        <MarkdownEditor resource={resource} onSave={onSaveMarkdown} onCancel={onCancel}
-                        onDelete={props.deleteSection && onDelete} />
-      );
-    }
-
-    const textWidth = readOnly ? 12 : 11;
-    return <Grid container justify="center">
-             <Grid item xs={textWidth} style={{padding:"1px"}}>
-               <MarkdownViewer resource={resource} />
-             </Grid>
-             { !readOnly &&
-               <Grid item xs={1}>
-                 <IconButton size="small" variant="contained" onClick={(e) => {setEditing(true);}}>
-                   <EditIcon />
-                 </IconButton>
-               </Grid> 
-             }
-           </Grid>;
+    const params = {onDelete, setEditing};
+    return <BlogSectionMarkdown {...props} {...params}/>;
   }
   return <div/>;
 }
 
 BlogSection.propTypes = {
-  insertImage: PropTypes.func.isRequired,
-  saveSection: PropTypes.func.isRequired,
   resource: PropTypes.object.isRequired,
   pathArticle: PropTypes.string.isRequired,
 };
