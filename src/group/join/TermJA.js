8feb18f575d9c69a5065fabeb86199521d2fbe6f
Isamu Arimoto <isamu@to-kyo.to>
2021-11-21 06:53:47 +0900

    [ts-migrate][src] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

---
+++ src/group/join/TermJA.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+});
+
+function Term(props) {
+
+  return <div/>;
+
+}
+
+export default withStyles(styles)(Term);
